0f4709dad79de4a6dd3ca32e657c126e069a2cad
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2022-01-31 23:38:44 +0600

    Sign in and signup page desgin completed with responsive

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+
+var container = document.getElementById('container');
+function sign_up_form() {
+  container.classList.add("signup_mode");
+}
+
+function sign_in_form() {
+  container.classList.remove("signup_mode");
+
+}
